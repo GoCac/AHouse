edc902b9b2fbf2917d4993fa77bf63c9ea5c20e9
nannan <759010589@qq.com>
2016-05-09 14:41:49 +0800

    优化MapViewController，添加CircleViewController

---
+++ House/CircleViewController.h
@@ -0,0 +1,13 @@
+//
+//  CircleViewController.h
+//  House
+//
+//  Created by dlut on 16/5/9.
+//  Copyright © 2016年 dlut. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CircleViewController : BaseViewController
+
+@end
