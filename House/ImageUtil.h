ad18685ef98dc2568a9c4ce36a5a772d1c5ab775
nannan <759010589@qq.com>
2016-05-12 16:37:02 +0800

    完善HouseDetailController,添加ImageUtil

---
+++ House/ImageUtil.h
@@ -0,0 +1,13 @@
+//
+//  ImageUtil.h
+//  House
+//
+//  Created by dlut on 16/5/12.
+//  Copyright © 2016年 dlut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageUtil : NSObject
+
+@end
---
+++ House/ImageUtil.m
@@ -0,0 +1,13 @@
+//
+//  ImageUtil.m
+//  House
+//
+//  Created by dlut on 16/5/12.
+//  Copyright © 2016年 dlut. All rights reserved.
+//
+
+#import "ImageUtil.h"
+
+@implementation ImageUtil
+
+@end
